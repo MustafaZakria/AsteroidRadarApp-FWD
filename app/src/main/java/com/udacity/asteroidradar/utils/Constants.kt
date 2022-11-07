97f504c36079c65774309eb1e34a1a9adf3cc549
Mustafa Zakria <mustafa.zakria21@gmail.com>
2022-11-08 01:43:58 +0200

    removed api key

--- app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/utils/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov"
-    const val API_KEY = "ca6Ohx5uwPMCvHtVtN4cFZ4VGsO9GO5RC3OAHtly"
+    const val API_KEY = ""
     const val WORK_NAME = "RefreshDataWorker"
 
 }
